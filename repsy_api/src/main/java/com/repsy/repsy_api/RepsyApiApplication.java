16167af12ee030fdcf669450b910b8128990c42d
atalay <atalaybeyazit16@gmail.com>
2025-04-25 16:26:27 +0300

    It seems

---
+++ repsy_api/src/main/java/com/repsy/repsy_api/RepsyApiApplication.java
@@ -0,0 +1,13 @@
+package com.repsy.repsy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepsyApiApplication.class, args);
+	}
+
+}
---
+++ repsy_api/src/test/java/com/repsy/repsy_api/RepsyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repsy.repsy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepsyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
